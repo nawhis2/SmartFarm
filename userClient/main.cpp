1012e9338bfeb32c301a01e9ace7fb8f83972613
nawhis2 <ps496@naver.com>
2025-07-01 02:41:39 +0100

    modify : RTSP Image processing streaming by Qt

--- userClient/main.cpp
+++ userClient/main.cpp
@@ -1,10 +1,53 @@
-// main.cpp
 #include <QApplication>
-#include "mainwindow.h"
-#include <opencv2/opencv.hpp>
-using namespace cv;
+#include <QLabel>
+#include <QTimer>
+#include <QImage>
+#include <QVBoxLayout>
+#include <QWidget>
+#include <mainwindow.h>
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+
+GstElement *pipeline = nullptr;
+GstElement *appsink = nullptr;
+QLabel *videoLabel = nullptr;
+
+QImage gstSampleToQImage(GstSample *sample) {
+    if (!sample) return QImage();
+
+    GstBuffer *buffer = gst_sample_get_buffer(sample);
+    GstCaps *caps = gst_sample_get_caps(sample);
+    if (!buffer || !caps) return QImage();
+
+    GstStructure *s = gst_caps_get_structure(caps, 0);
+    int width, height;
+    gst_structure_get_int(s, "width", &width);
+    gst_structure_get_int(s, "height", &height);
+
+    GstMapInfo map;
+    if (!gst_buffer_map(buffer, &map, GST_MAP_READ)) return QImage();
+
+    QImage img((uchar *)map.data, width, height, QImage::Format_RGB888);
+    QImage copy = img.copy();
+
+    gst_buffer_unmap(buffer, &map);
+    return copy;
+}
+
+void updateFrame() {
+    GstSample *sample = gst_app_sink_try_pull_sample(GST_APP_SINK(appsink), 10000);
+    if (!sample) return;
+
+    QImage img = gstSampleToQImage(sample);
+    if (!img.isNull() && videoLabel) {
+        videoLabel->setPixmap(QPixmap::fromImage(img).scaled(videoLabel->size(), Qt::KeepAspectRatio));
+    }
+
+    gst_sample_unref(sample);
+}
 
 int main(int argc, char *argv[]) {
+    gst_init(nullptr, nullptr);
     QApplication app(argc, argv);
     MainWindow w;
     w.setWindowTitle("🌱 스마트팜 통합 모니터링 시스템");
--- userClient/mainwindow.h
+++ userClient/mainwindow.h
@@ -12,7 +12,7 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 signals:
